f1dc8b3d46e844dc6f18d6e7ac524c094ac46222
= <=>
2022-02-14 15:12:02 +0300

    Azamat in Comits

--- src/main/java/peaksoft/model/AnimalsCage.java
+++ src/main/java/peaksoft/model/AnimalsCage.java
@@ -1,19 +1,38 @@
 package peaksoft.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("singleton")
 public class AnimalsCage {
 
-    @Autowired
+    private Timer timer;
+
     private Animal animal;
+@Autowired
+@Qualifier("dog")
+    public void setAnimal(Animal animal) {
+        this.animal = animal;
+    }
+
+
+    public Timer getTimer() {
+        return timer;
+    }
+@Autowired
+@Qualifier("timer")
+    public void setTimer(Timer timer) {
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 }
